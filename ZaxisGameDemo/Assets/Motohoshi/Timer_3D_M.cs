1d087356adc454c5df32c947513c05f2b243b3c6
kamotohashi <32289149+kamotohashi@users.noreply.github.com>
2018-08-01 18:13:13 +0900

    unko

--- ZaxisGameDemo/Assets/Motohoshi/Timer_3D_M.cs
+++ ZaxisGameDemo/Assets/Motohoshi/Timer_3D_M.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer_3D_M : MonoBehaviour {
+    int FullTimer = 10;
+    int currentTimer = 10; 
 
 	// Use this for initialization
 	void Start () {
